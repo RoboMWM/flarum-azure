d0acb23f63dc874c0ce6094cae2427f73d4cb76d
RoboMWM <RoboMWM@users.noreply.github.com>
2016-03-16 21:06:33 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-##################################################################################################################################
- # Download Composer
- # ----------
- echo Downloading Composer
- curl -sS https://getcomposer.org/installer | php
- 
- ##################################################################################################################################
- # Dependency install
- # ----------
-  
  # Invoke Composer in the deployment directory
  rm -rf $DEPLOYMENT_TARGET
  echo Invoking composer install in deployment directory $DEPLOYMENT_TARGET
- php composer.phar create-project flarum/flarum $DEPLOYMENT_TARGET --stability=beta --no-interaction
+ composer create-project flarum/flarum $DEPLOYMENT_TARGET --stability=beta --no-interaction
+ cd $DEPLOYMENT_TARGET
+ echo chmod'ing the following directory
+ pwd
+ chmod -R 755 .
+ ls -a
+ chmod -R 755 *
+ ls -a
+ chmod -R 755 *.*
+ ls -a
+ chmod -R $DEPLOYMENT_TARGET
  
 echo "Finished successfully."
